fe503315086d79b8e6bd150c4fe71e763a2a2e28
António Ramadas <up201303568@gcloud.fe.up.pt>
2015-12-07 18:51:36 +0000

    Estrutura do trabalho
    Falta estruturar o prolog (mas está subentendido com a planificação que
    fiz)

--- Proj3/reader/Board.js
+++ Proj3/reader/Board.js
@@ -1,8 +1,13 @@
 Board.prototype.constructor=Board;
 
 function Board() {
-    
+   
 var str = "[[[[-1,-1],[0,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],[[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],[[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[0,-1],[-1,-1],[0,-1]],[[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[0,-1]],[[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],[[-1,-1],[-1,-1],[0,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],[[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],[[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]]],[[14,18],[14,18]],player1,playerVSplayer,5,[]]";
+
+	//o request vai ter que devolver qual foi a jogada (a peca movida)
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//temos de falar sobre como e que vamos dizer qual peca
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 	this.nextReadIndex;
 
@@ -11,6 +16,24 @@ var str = "[[[[-1,-1],[0,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1],[-1,-1]],[[
 	
 	var pieces = this.getNumPiecesFromRequest(str);
 	console.log(pieces);
+
+	var playerTurn = this.getPlayerTurn(str);
+	console.log(playerTurn);
+
+	//endPoints é o número de Pontos em que o jogo acaba
+	//level1 e level2 e o nivel de dificuldade do jogador 1 e 2
+	//typeOfGame pode ser humanoVShumano, playerVSplayer, ...
+	//timeForEachPlayer e o tempo para cada jogador (se acabar o tempo, passa a vez)
+	var endPoints, level1, level2, typeOfGame, timeForEachPlayer;
+
+	//so sao guardadas as jogadas e tabuleiros validos
+	//para depois fazer o filme (com animacao) basta chamar o prolog com a moveList
+	//para depois fazer undo e so ir buscar o ultimo elemento do array
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//(temos de falar sobre quando e que o undo e possivel!!!)
+	//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	var boardsList = []; //Exemplo: boardsList[0] = tabuleiro
+	var movesList = []; //Exemplo: movesList[0] = [Peca, jogador] (temos de falar como guardamos a peca/jogada)
 };
 
 Board.prototype.getBoardFromRequest = function(request) {
@@ -28,4 +51,10 @@ Board.prototype.getNumPiecesFromRequest = function(request) {
 	this.nextReadIndex += (match[0].length+1);
 	var pieces = JSON.parse(match[0]);
 	return pieces;
+};
+
+Board.prototype.getPlayerTurn = function(request) {
+	var player = parseInt(request[this.nextReadIndex+6]);
+	this.nextReadIndex += ("playerX".length+1);
+	return player;
 };
